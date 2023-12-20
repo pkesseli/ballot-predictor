48236e9bad6adbf77fc9133aa5535eae16b50696
Pascal Kesseli <pascal_kesseli@hotmail.com>
2023-12-20 01:51:08 +0100

    Extend result entity
    Extend ballot result by status.

--- src/python/bp/entity/ballot.py
+++ src/python/bp/entity/ballot.py
@@ -1,18 +1,42 @@
 from bp.entity.bill import Bill
 from bp.entity.result import DoubleMajorityBallotResult
 
+from enum import Enum
+
+
+class BallotStatus(Enum):
+    """Simplification of all possible real-world states in which a ballot could
+    be. In reality, ballots may fail for a variety of reasons without vote, but
+    we summarise them into one failing state at the moment due to small sample
+    sizes for the various subcategories.
+    """
+    PENDING = 1
+    """No vote was held on the measure yet. This is also used for ballots where
+    we would like to generate a prediction.
+    """
+    FAILED = 1
+    """The measure failed without a vote."""
+    COMPLETED = 2
+    """A vote was held on the measure, implying that a respective result is
+    available.
+    """
+
 
 class DoubleMajorityBallot:
     """Double majority ballot consisting of bill details information and an
     optional result, if the ballot has already taken place.
     """
 
-    def __init__(self, bill: Bill, result: DoubleMajorityBallotResult | None) -> None:
+    def __init__(self, bill: Bill, status: BallotStatus, result: DoubleMajorityBallotResult | None) -> None:
         """Initialises the ballot with all properties.
 
         Args:
             bill (Bill): Details information about bill on the ballot.
+            status (BallotStatus): Indicates whether a ballot is still pending,
+            was already held actually held or wheher the measure failed without
+            vote.
             result (DoubleMajorityBallotResult | None): Optional ballot result.
         """
         self.bill = bill
+        self.status = status
         self.result = result
--- src/python/bp/entity/result.py
+++ src/python/bp/entity/result.py
@@ -5,5 +5,11 @@ class DoubleMajorityBallotResult:
     """Double majority ballot result."""
 
     def __init__(self, percentageYes: Decimal, acceptingCantons: Decimal) -> None:
+        """Ballot voting result for a double majority vote.
+
+        Args:
+            percentageYes (Decimal): Share of population voting yes.
+            acceptingCantons (Decimal): Share of cantons voting yes.
+        """
         self.percentageYes = percentageYes
         self.acceptingCantons = acceptingCantons
