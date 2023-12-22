b37c63e7059dc548a371aa4fbbed68ed71a18501
Pascal Kesseli <pascal_kesseli@hotmail.com>
2023-12-22 13:41:00 +0100

    Add OpenThesaurus
    Bundle OpenThesaurus for data augmentation.

---
+++ src/python/bp/data/tests/test_openthesaurus.py
@@ -0,0 +1,32 @@
+from bp.data.openthesaurus import OpenThesaurus
+
+import os
+import tempfile
+import unittest
+
+from typing import List
+
+
+class TestOpenthesaurus(unittest.IsolatedAsyncioTestCase):
+
+    async def test_create_database_if_not_exists(self):
+        database: str
+        with tempfile.NamedTemporaryFile() as temp_file_generator:
+            database = temp_file_generator.name
+
+        try:
+            async with OpenThesaurus(database):
+                self.assertGreaterEqual(os.path.getsize(database), 50 << 20)
+        finally:
+            os.remove(database)
+
+    async def test_find_synonyms(self):
+        async with OpenThesaurus() as thesaurus:
+            synonyms: List[str] = await thesaurus.find_synonyms("Junge")
+            self.assertCountEqual(["Knirps", "Knabe", "Bub", "Wicht", "Bube", "Pimpf", "Steppke", "Kerlchen",
+                                  "Jungchen", "Bengel", "Kleiner", "Kurzer", "Bauer", "Bube", "Unter", "Wuenscher"], synonyms)
+
+    async def test_find_antonyms(self):
+        async with OpenThesaurus() as thesaurus:
+            antonyms: List[str] = await thesaurus.find_antonyms("Mann")
+            self.assertCountEqual(["Frau"], antonyms)
