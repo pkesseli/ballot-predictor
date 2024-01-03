096f643ac8c36b442388cf3c65f01b80be3aef4d
Pascal Kesseli <pascal_kesseli@hotmail.com>
2024-01-03 16:13:49 +0100

    Persist model
    Add ability to persist and load trained model for incremental model training.

--- src/python/bp/train/train.py
+++ src/python/bp/train/train.py
@@ -9,23 +9,24 @@ import asyncio
 
 
 async def main():
-    """Helper script to train our vote result rediction model. The model is
-    based on a multilingual BERT base model, extended by an input layer
-    suitable for consuming our tokenized bills and an ouput layer producing the
-    predicted vote results. Excluded from unit test coverage check, since this
-    script is only executed manually during experimental and training
-    preparations.
+    """Helper script to train our vote result prediction model using
+    resources/bk.admin.ch/augmented-initiatives.json and save it in
+    resources/tensorflow/vote-result-prediction.keras. The model is based on a
+    multilingual BERT base model, extended by an input layer suitable for
+    consuming our tokenized bills and an ouput layer producing the predicted
+    vote results. Excluded from unit test coverage check, since this script is
+    only executed manually during experiments.
     """
-    bert = VoteResultPredictionModel()
-
+    model = VoteResultPredictionModel()
     ballots: List[DoubleMajorityBallot] = await Serialisation.load_augmented_initiatives()
-    features: Tensor = bert.create_bill_features(
+    features: Tensor = model.create_bill_features(
         [ballot.bill for ballot in ballots])
-    labels: Tensor = bert.create_double_majority_labels(
+    labels: Tensor = model.create_double_majority_labels(
         [ballot.result for ballot in ballots])
     training_dataset: tf.data.Dataset = tf.data.Dataset.from_tensor_slices(
         (features, labels))
-    bert.train(training_dataset)
+    model.train(training_dataset, 10)
+    model.save()
 
 
 if __name__ == "__main__":
